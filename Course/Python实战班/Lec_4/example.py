91508750cbfd58c56782148ad29566e6cd8c8e89
mingma1369 <mingma1369@gmail.com>
2023-02-27 09:06:37 -0500

    update

---
+++ Course/Python实战班/Lec_4/example.py
@@ -0,0 +1,9 @@
+# Python Module example
+# saved in /Users/kevin/Documents/GitHub/Python/Course/Python实战班/In_Class
+
+def add(a, b):
+    """ This program adds two numbers
+    and return the result"""
+
+    result = a + b
+    return result
